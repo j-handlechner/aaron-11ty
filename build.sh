924f41356711147df29b869d870fea5c993b0b58
Johannes Handlechner <jhandlechner.mmt-b2021@fh-salzburg.ac.at>
2022-10-27 21:03:04 +0200

    update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 npx sass ./src/sass/style.scss ./dist/style.css
-cp ./src/index.html ./dist/
 cp ./src/favicons/*.* ./dist/
 cp -R ./src/images ./dist/
 cp -R ./src/fonts ./dist/fonts
 npx esbuild ./src/js/main.js --bundle --outfile=./dist/main.js  --minify
+npx browser-sync reload -u http://localhost:8080 # to manually reload (wsl problem with eleventy)
