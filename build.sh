482f660f1bb21004322f32958d5db4437fd21792
Johannes Handlechner <jhandlechner.mmt-b2021@fh-salzburg.ac.at>
2022-12-29 23:11:34 +0100

    apply feedback

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ cp ./src/favicons/*.* ./dist/
 cp -R ./src/images ./dist/
 cp -R ./src/fonts ./dist/fonts
 npx esbuild ./src/js/main.js --bundle --outfile=./dist/main.js  --minify
+npx @11ty/eleventy --input=src --output=dist
 npx browser-sync reload -u http://localhost:8080 # to manually reload (wsl problem with eleventy)
